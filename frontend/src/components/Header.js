2a644fa9f49ce94e9347ef0b82b4e79d4c9d7f48
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-11-08 19:48:38 +0900

    create: added `react-router-dom` and `react-router-bootstrap` and set `Routes`, `Router`, `Link`, `LinkContainer`

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
 import { Navbar, Nav, Container } from 'react-bootstrap';
 
 const Header = () => {
@@ -6,20 +7,26 @@ const Header = () => {
         <header>
             <Navbar bg='dark' variant='dark' expand="lg" collapseOnSelect>
                 <Container>
-                    <Navbar.Brand href="/">YYShop</Navbar.Brand>
+                    <LinkContainer to='/'>
+                        <Navbar.Brand>YYShop</Navbar.Brand>
+                    </LinkContainer>
                     <Navbar.Toggle aria-controls="basic-navbar-nav" />
                     <Navbar.Collapse id="basic-navbar-nav">
                         <Nav className="ml-auto">
-                            <Nav.Link href="/cart">
-                                <i className='fas fa-shopping-cart'>
-                                    Cart
-                                </i>
-                            </Nav.Link>
-                            <Nav.Link href="/login">
-                                <i className='fas fa-user'>
-                                    Sign In
-                                </i>
-                            </Nav.Link>
+                            <LinkContainer to='/cart'>
+                                <Nav.Link >
+                                    <i className='fas fa-shopping-cart'>
+                                        Cart
+                                    </i>
+                                </Nav.Link>
+                            </LinkContainer>
+                            <LinkContainer to='/login'>
+                                <Nav.Link>
+                                    <i className='fas fa-user'>
+                                        Sign In
+                                    </i>
+                                </Nav.Link>
+                            </LinkContainer>
                         </Nav>
                     </Navbar.Collapse>
                 </Container>
