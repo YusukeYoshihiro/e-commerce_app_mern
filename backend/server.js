d2f2f7814d474ff372650fc6b458565f4e9952e0
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-11-14 21:39:37 +0900

    refactor: modified error handling for API request

--- backend/server.js
+++ backend/server.js
@@ -9,13 +9,29 @@ connectDB();
 
 const app = express();
 
+app.use((err, eq, res, next) => {
+    const error = new Error(`Not Found - ${req.originalUrl}`);
+    res.status(404);
+    next(error);
+})
+
+app.use((err, req, res, next) => {
+    const statusCode = res.statusCode === 200 ? res.statusCode : 500;
+    res.status(statusCode);
+    res.json({
+        message: err.message,
+        stack: process.env.NODE_ENV === "production" ? null : err.stack,
+    });
+})
+
+
 app.get('/', (req, res) => {
     res.send('API is running...');
 })
 
 app.use('/api/products', productRoutes);
 
-const PORT = process.env.PORT || 8029;
+const PORT = process.env.PORT || 8001;
 
 app.listen(
     PORT,
