04ae6693ed38e1034b6488e83690c7cc049b200a
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-11-22 12:36:40 +0900

    implement user register part

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -2,7 +2,10 @@ import {
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
-    USER_LOGOUT
+    USER_LOGOUT,
+    USER_REGISTER_FAIL,
+    USER_REGISTER_REQUEST,
+    USER_REGISTER_SUCCESS
 } from "../constants/userConstants"
 
 export const userLoginReducer = (
@@ -18,6 +21,22 @@ export const userLoginReducer = (
             return { loading: false, error: payload }
         case USER_LOGOUT:
             return {}
+        default:
+            return state
+    }
+}
+
+export const userRegisterReducer = (
+    state = {},
+    { type, payload }
+) => {
+    switch (type) {
+        case USER_REGISTER_REQUEST:
+            return { loading: true }
+        case USER_REGISTER_SUCCESS:
+            return { loading: false, userInfo: payload }
+        case USER_REGISTER_FAIL:
+            return { loading: false, error: payload }
         default:
             return state
     }
