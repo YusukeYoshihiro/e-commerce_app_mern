3e3a170ccb59655048877e95d4e0696f55a330d4
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-11-30 16:19:35 +0900

    feat: implement stripe checkout process

--- backend/server.js
+++ backend/server.js
@@ -4,7 +4,7 @@ import connectDB from './config/db.js';
 import productRoutes from './routes/productRoutes.js';
 import userRoutes from './routes/userRoutes.js';
 import orderRoutes from './routes/orderRoutes.js';
-import billingRoutes from './routes/billingRoutes.js'
+import billingRoutes from './routes/billingRoutes.js';
 import { notFound, errorHandler } from './middleware/errorMiddleware.js'
 
 dotenv.config();
