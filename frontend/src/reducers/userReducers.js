7b1f05307092c7e1892fef63ccf6c0ac181d3868
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-12-16 00:33:48 +0900

    feat: implemented my order part with redux

--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,6 +1,7 @@
 import {
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
+    USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
@@ -60,6 +61,8 @@ export const userDetailsReducer = (
             return { loading: false, user: payload }
         case USER_DETAILS_FAIL:
             return { loading: false, error: payload }
+        case USER_DETAILS_RESET:
+            return { user: {} }
         default:
             return state
     }
