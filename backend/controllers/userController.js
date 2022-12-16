bc95047054e86326df7925f5dd035670de175f09
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2022-12-16 17:14:18 +0900

    feat: implement admin screen to SHOW(GET) user list, DELETE user

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -124,4 +124,21 @@ const getUsers = asyncHandler(asyncHandler(async (req, res) => {
     res.json(users)
 }))
 
-export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers }
+/**
+ * @desc    Delete delete user
+ * @route   DELETE /api/users/:id
+ * @access  Private/Admin
+ */
+const deleteUser = asyncHandler(asyncHandler(async (req, res) => {
+    const user = await User.findById(req.params.id)
+
+    if (user) {
+        await user.remove()
+        res.json({ message: 'User removed' })
+    } else {
+        res.status(404);
+        throw new Error('User Not Found');
+    }
+}))
+
+export { authUser, registerUser, getUserProfile, updateUserProfile, getUsers, deleteUser }
--- frontend/src/reducers/userReducers.js
+++ frontend/src/reducers/userReducers.js
@@ -1,8 +1,15 @@
 import {
+    USER_DELETE_FAIL,
+    USER_DELETE_REQUEST,
+    USER_DELETE_SUCCESS,
     USER_DETAILS_FAIL,
     USER_DETAILS_REQUEST,
     USER_DETAILS_RESET,
     USER_DETAILS_SUCCESS,
+    USER_LIST_FAIL,
+    USER_LIST_REQUEST,
+    USER_LIST_RESET,
+    USER_LIST_SUCCESS,
     USER_LOGIN_FAIL,
     USER_LOGIN_REQUEST,
     USER_LOGIN_SUCCESS,
@@ -81,6 +88,40 @@ export const userUpdateProfileReducer = (
             return { loading: false, error: payload }
         case USER_UPDATE_PROFILE_RESET:
             return {}
+        default:
+            return state
+    }
+}
+
+export const userListReducer = (
+    state = { users: [] },
+    { type, payload }
+) => {
+    switch (type) {
+        case USER_LIST_REQUEST:
+            return { loading: true }
+        case USER_LIST_SUCCESS:
+            return { loading: false, users: payload }
+        case USER_LIST_FAIL:
+            return { loading: false, error: payload }
+        case USER_LIST_RESET:
+            return { users: [] }
+        default:
+            return state
+    }
+}
+
+export const userDeleteReducer = (
+    state = {},
+    { type, payload }
+) => {
+    switch (type) {
+        case USER_DELETE_REQUEST:
+            return { loading: true }
+        case USER_DELETE_SUCCESS:
+            return { loading: false, success: true }
+        case USER_DELETE_FAIL:
+            return { loading: false, error: payload }
         default:
             return state
     }
