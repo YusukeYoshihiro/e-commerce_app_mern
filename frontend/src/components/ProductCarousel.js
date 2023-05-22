8764fb87b4f1db8d794e406289b565a7e6d8bbfe
YusukeYoshihiro <yusuke.10.25.61@gmail,com>
2023-05-23 03:57:43 +0900

    fix: changed text color for Carousel Caption text

--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -1,6 +1,6 @@
 import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
-import { Carousel, Image } from 'react-bootstrap';
+import { Carousel, Col, Image } from 'react-bootstrap';
 import { useDispatch, useSelector } from 'react-redux';
 import Loader from './Loader';
 import Message from './Message';
@@ -23,9 +23,11 @@ const ProductCarousel = () => {
                     {products.map(product => (
                         <Carousel.Item key={product._id}>
                             <Link to={`/product/${product._id}`}>
-                                <Image src={product.image} alt={product.name} fluid />
+                                <Col sm={10} md={10} xs={9}>
+                                    <Image src={product.image} alt={product.name} fluid />
+                                </Col>
                                 <Carousel.Caption className='carousel-caption'>
-                                    <h2>{product.name} ($ {product.price})</h2>
+                                        <h2 className='carousel_caption_text'>{product.name} ($ {product.price})</h2>
                                 </Carousel.Caption>
                             </Link>
                         </Carousel.Item>
